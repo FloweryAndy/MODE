d38d98cffbd0fef1abc170d8d64bd86b7ab1051e
adm9835 <andy_moody_7@yahoo.com>
2023-11-29 22:19:45 -0500

    finish choco spikes

--- object/candy/chocolate_spikes.gd
+++ object/candy/chocolate_spikes.gd
@@ -12,26 +12,41 @@ var expanded = false
 
 
 func _ready() -> void:
-	pass
+	visible = false
+	monitoring = false
+	body_entered.connect(_on_body_entered)
 
 
 func _process(_delta: float) -> void:
-	if shape_cast2d.is_colliding() and can_act and !expanded:
-		can_act = false
-		animation_player.play("expand")
-		await animation_player.animation_finished
-		can_act = true
-	elif !shape_cast2d.is_colliding() and can_act and expanded:
-		can_act = false
-		animation_player.play_backwards("expand")
-		await animation_player.animation_finished
-		can_act = true
+	if visible:
+		if shape_cast2d.is_colliding() and can_act and !expanded:
+			await get_tree().create_timer(0.5).timeout
+			can_act = false
+			animation_player.play("expand")
+			await animation_player.animation_finished
+			if visible:
+				monitoring = true
+			expanded = true
+			can_act = true
+		elif !shape_cast2d.is_colliding() and can_act and expanded:
+			await get_tree().create_timer(0.5).timeout
+			can_act = false
+			animation_player.play_backwards("expand")
+			monitoring = false
+			await animation_player.animation_finished
+			expanded = false
+			can_act = true
 
 
 func change_mode(new_mode: int) -> void:
 	if new_mode == my_mode:
 		visible = true
-		collision_shape2d.disabled = false
 	else:
+		monitoring = false
 		visible = false
-		collision_shape2d.disabled = true
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		body.hurt(10, sprite2d.global_position)
+		monitoring = false
