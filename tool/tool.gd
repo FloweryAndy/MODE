79756e576ac8f5db2508981b0d838dc525b8c53d
adm9835 <andy_moody_7@yahoo.com>
2023-11-29 20:04:14 -0500

    fix breakable

--- tool/tool.gd
+++ tool/tool.gd
@@ -27,6 +27,7 @@ var current_mode = 0
 
 func _ready():
 	collision_shape2d.disabled = true
+	connect("body_entered", Callable(_on_body_entered))
 
 
 func _process(_delta):
@@ -84,3 +85,9 @@ func special() -> void:
 		Tool.GRAVITYGUN:
 			pass
 	can_act = true
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.has_method("breakable"):
+		if current_mode == body.my_mode:
+			body.breakable()
