a5489aa60bd53c98f872e005fb3369f94b759e0f
adm9835 <andy_moody_7@yahoo.com>
2023-12-01 14:06:21 -0500

    start vine, implement dark light

--- main.gd
+++ main.gd
@@ -13,7 +13,19 @@ func _ready() -> void:
 
 func _input(event):
 	if event.is_action_pressed("change_mode"):
-		change_mode()
+		change_mode_debug()
+
+
+func change_mode_debug() -> void:
+	change_mode_timer.stop()
+	var new_mode = current_mode + 1
+	if new_mode > 11:
+		new_mode = 0
+	current_mode = new_mode
+	var changeable_nodes = get_tree().get_nodes_in_group("change_mode")
+	for i in changeable_nodes:
+		if i.has_method("change_mode"):
+			i.change_mode(current_mode)
 
 
 func change_mode() -> void:
